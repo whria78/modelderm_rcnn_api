ebf3b7c92cc125d162b2e8cfd7f19a665bc46226
whria78 <whria78@gmail.com>
2020-11-13 18:56:08 +0900

    Update test.py

--- test.py
+++ test.py
@@ -144,7 +144,6 @@ for img_no,img_path in enumerate(img_list):
             print("RESUME")
         elif error_flag!='success':
             print("Failed : %s" % (result_raw))
-            if "No lesion" in result_raw:continue
             sys.exit(1)
 
     ###
